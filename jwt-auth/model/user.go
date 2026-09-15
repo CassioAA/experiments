d8194b09aee052f6f73ed8a33a8b16fc757d089a
CassioAA <cassioalmeidaccti@gmail.com>
2026-09-15 14:15:54 -0300

    chore: model, file was not saved

--- jwt-auth/model/user.go
+++ jwt-auth/model/user.go
@@ -1,1 +1,12 @@
 package model
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	Activate  bool      `json:"activate"` // if suspending or pending verification
+	CreatedAt time.Time `json:"created_at"`
+}
